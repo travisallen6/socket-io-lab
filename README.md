aca499416532feaf57dcd905f623300f417b9bd4
Travis Allen <travisallen6@gmail.com>
2018-08-08 10:32:21 -0600

    Cleaned up for push

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-## NGROK
-ngrok http --host-header=rewrite 3010
+NGROK - A way to get a live accessible URL that points to a port on localhost. - https://ngrok.com/
+
+
+Mason's example repo - https://github.com/masongalland/simple-socket-chat - Simple chat app
+Beth's example repo - https://github.com/Be-The-Bert/react-sockets-demo - Rudimentary chat app, with basic, broadcast, and room socket functionality.
+Socket.io Docs - https://socket.io/docs/
+Video - https://www.youtube.com/watch?v=9FPkN6ETqes&feature=youtu.be&t=23m39s - Walkthrough video on using Socket.io with React.
+Nate's chat app including basic, broadcast, and rooms - https://github.com/nathan-08/web-socket-chat-app
+Nate's socket countdown example - https://github.com/nathan-08/socket-io-countdown-timer
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,6 @@ import io from 'socket.io-client';
 import SetUsername from './SetUsername';
 import UserList from './UserList';
 import MessageBox from './MessageBox';
-import Tabs from './Tabs';
 import Composer from './Composer';
 
 
@@ -20,7 +19,7 @@ class Chat extends Component {
   }
 
   componentDidMount(){
-        this.socket = io('http://localhost:3010'); // Remove when not running two servers
+        this.socket = io(); // Remove when front end is hosted using express.static()
 
         this.socket.on('message', (message) => {
             this.setState({messages: [message, ...this.state.messages]})
@@ -75,30 +74,6 @@ class Chat extends Component {
         })
         this.socket.emit('username', userInput) 
     }
-
-    // toggleSelected = (id) => {
-    //     const newUsers = this.state.users.map( user => {
-    //         if(user.id === id) user.selected = !user.selected;
-    //         return user;
-    //     })
-    //     this.setState({users: newUsers})
-    // }
-
-    // updateRoom = (bool) => {
-    //     this.setState({tabSecret: bool})
-    // }
-
-    // joinRoom = () => {
-    //     const {joinedSecret} = this.state
-    //     if(!joinedSecret){
-    //         this.socket.emit('join', 'secret')
-    //     } else {
-    //         this.socket.emit('leave', 'secret')
-    //     }
-    //     this.setState({
-    //         joinedSecret: !joinedSecret
-    //     })
-    // }
 
     render() {
         return (
